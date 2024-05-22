137963e465933ced2efe11774eecd8a0545ec45c
vinsen <vinsen.tandra@itbss.ac.id>
2024-05-22 09:27:44 +0700

    create penjual table

--- resources/views/layout/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="py-4 bg-light mt-auto">
-    <div class="container-fluid">
-        <div class="d-flex align-items-center justify-content-between small">
-            <div class="text-muted">Copyright &copy; Chicken World 2024</div>
-        </div>
-    </div>
-</footer>
