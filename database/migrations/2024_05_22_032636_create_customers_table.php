9b80259976c07e73f0af1b59e7b516e9443771c2
vinsen <vinsen.tandra@itbss.ac.id>
2024-05-22 10:29:26 +0700

    update customers table

---
+++ database/migrations/2024_05_22_032636_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('customer_id')->unsigned();
+            $table->string('customer_name', 50);
+            $table->string('akun_customer', 50);
+            $table->string('nomer_telepon', 50);
+            $table->string('alamat', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
