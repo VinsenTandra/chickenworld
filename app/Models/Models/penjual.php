6bd9898b83f787278b6248dbc8065f420b7a36f1
VincentWilliamson <vincent.williamson@itbss.ac.id>
2024-06-04 15:28:09 +0700

    penjual dan penjualan

--- app/Models/Models/penjual.php
+++ app/Models/Models/penjual.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class penjual extends Model
 {
     use HasFactory;
+
+    public $table = 'penjual';
+
+const CREATED_AT = 'created_at';
+const UPDATED_AT = 'updated_at';
+
+protected $auditTimestamps = true;
+
+public $fillable = [
+'id',
+'owner_id',
+'ind_staff',
+'nama',
+];
+
 }
--- app/Models/Models/penjualan.php
+++ app/Models/Models/penjualan.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class penjualan extends Model
 {
     use HasFactory;
+
+    public $table = 'penjualans';
+
+const CREATED_AT = 'created_at';
+const UPDATED_AT = 'updated_at';
+
+protected $auditTimestamps = true;
+
+public $fillable = [
+'id',
+'id_penjualan',
+'customer_id',
+'id_staff',
+'link_website',
+'tanggal_pengambilan',
+'jumlah_harga',
+];
 }
