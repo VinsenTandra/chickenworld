24a7b646cdb10a84d92a8187a7bf056023b100f5
mathew <mathewabel.nugroho@itbss.ac.id>
2024-06-05 08:18:03 +0700

    membuat model produk dan stok

--- app/Models/Models/product.php
+++ app/Models/Models/product.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class product extends Model
 {
     use HasFactory;
+
+    public $table = 'penjualans';
+
+const CREATED_AT = 'created_at';
+const UPDATED_AT = 'updated_at';
+
+protected $auditTimestamps = true;
+
+public $fillable = [
+'id',
+'kode_produk',
+'nama_produk',
+'harga',
+];
 }
--- app/Models/Models/stock.php
+++ app/Models/Models/stock.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class stock extends Model
 {
     use HasFactory;
+
+    public $table = 'penjualans';
+
+const CREATED_AT = 'created_at';
+const UPDATED_AT = 'updated_at';
+
+protected $auditTimestamps = true;
+
+public $fillable = [
+'id',
+'id_penjualan',
+'kode_produk',
+'Form_PO',
+'jumlah',
+];
 }
